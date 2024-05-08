7d82fe2473bbc6f2f529a3cda1945d4280dfe605
AlbaytSolo <solo.albayt@gmail.com>
2024-05-08 12:55:35 +0700

    lesgo

--- app/Paket/page.tsx
+++ app/Paket/page.tsx
@@ -1,7 +1,7 @@
 import CardPaket from "@/components/CardPaket";
 import SectionHead from "@/components/SectionHead";
 import { PACKAGE_DATA } from "@/constant";
-import Link from "next/link";
+import data_paket from "@/constant/datapaket.json"
 import React from "react";
 
 const page = () => {
@@ -10,71 +10,14 @@ const page = () => {
       <section className="bg-white p-6 rounded-lg">
         <SectionHead title="Paket" placeholder="Cari Paket" />
 
-        <table className="w-full">
-          <thead>
-            <tr className="bg-[#f14310] text-white">
-              <th className="p-2 font-semibold text-[14px]">ID Paket</th>
-              <th className="p-2 font-semibold text-[14px]">Judul</th>
-              <th className="p-2 font-semibold text-[14px]">Harga</th>
-              <th className="p-2 font-semibold text-[14px]">Tipe</th>
-              <th className="p-2 font-semibold text-[14px]">Jadwal</th>
-              <th className="p-2 font-semibold text-[14px]">Lokasi Berangkat</th>
-              <th className="p-2 font-semibold text-[14px]">Hotel</th>
-              <th className="p-2 font-semibold text-[14px]"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {PACKAGE_DATA.map((paket) => (
-              <tr key={paket.key} className="border-b text-center">
-                <td className="p-2 text-[14px]">{paket.key}</td>
-                <td className="p-2 text-[14px]">{paket.title.length > 40 ? paket.title.slice(0, 40) + '...' : paket.title}</td>
-                <td className="p-2 text-[14px]">{paket.harga}</td>
-                <td className="p-2 text-[14px]">{paket.tipe}</td>
-                <td className="p-2 text-[14px]">{paket.jadwal}</td>
-                <td className="p-2 text-[14px]">{paket.lokasiberangkat}</td>
-                <td className="p-2 text-[14px]">{paket.hotel}</td>
-                <td className="p-2 text-[14px]">
-                  <div className="flex">
-                    <Link href={"/"}>
-                      <button className="w-8 h-8 p-1 duration-200 rounded-lg hover:bg-gray-20">
-                        <svg
-                          clip-rule="evenodd"
-                          fill-rule="evenodd"
-                          stroke-linejoin="round"
-                          stroke-miterlimit="2"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="m11.25 6c.398 0 .75.352.75.75 0 .414-.336.75-.75.75-1.505 0-7.75 0-7.75 0v12h17v-8.749c0-.414.336-.75.75-.75s.75.336.75.75v9.249c0 .621-.522 1-1 1h-18c-.48 0-1-.379-1-1v-13c0-.481.38-1 1-1zm1.521 9.689 9.012-9.012c.133-.133.217-.329.217-.532 0-.179-.065-.363-.218-.515l-2.423-2.415c-.143-.143-.333-.215-.522-.215s-.378.072-.523.215l-9.027 8.996c-.442 1.371-1.158 3.586-1.264 3.952-.126.433.198.834.572.834.41 0 .696-.099 4.176-1.308zm-2.258-2.392 1.17 1.171c-.704.232-1.274.418-1.729.566zm.968-1.154 7.356-7.331 1.347 1.342-7.346 7.347z"
-                            fill-rule="nonzero"
-                          />
-                        </svg>
-                      </button>
-                    </Link>
-                    <Link href={"/"}>
-                      <button className="w-8 h-8 p-1 duration-200 rounded-lg hover:bg-gray-20">
-                        <svg
-                          clip-rule="evenodd"
-                          fill-rule="evenodd"
-                          stroke-linejoin="round"
-                          stroke-miterlimit="2"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="m4.015 5.494h-.253c-.413 0-.747-.335-.747-.747s.334-.747.747-.747h5.253v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-.254v15.435c0 .591-.448 1.071-1 1.071-2.873 0-11.127 0-14 0-.552 0-1-.48-1-1.071zm14.5 0h-13v15.006h13zm-4.25 2.506c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm-4.5 0c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm3.75-4v-.5h-3v.5z"
-                            fill-rule="nonzero"
-                          />
-                        </svg>
-                      </button>
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {/* Content */}
+        <div className="grid grid-cols-3 gap-5">
+          {data_paket.map((paket) => (
+            <CardPaket key={paket.paketID} paketID={paket.paketID} img={paket.img} title={paket.title} durasi={paket.durasi} harga={paket.harga} harga_dp={paket.harga_dp} hotel={paket.hotel} jadwal={paket.jadwal} lokasikeberangkatan={paket.lokasiberangkat} maskapai={paket.maskapai} remainingseat={paket.remainingseat} thumbnail={paket.thumbnail} totalseat={paket.totalseat} />
+          ))}
+        </div>
+        {/* End Of Content */}
+
       </section>
     </>
   );
--- components/CardPaket.tsx
+++ components/CardPaket.tsx
@@ -1,207 +1,58 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link'
+import React from 'react'
 
-type CardProps = {
-  paket_id: string;
-  img: string;
-  harga: string;
-  tipe: string;
-  title: string;
-  jadwalBerangkat: string;
-  durasi: string;
-  hotel: number;
-  totalseat: number;
-  remainingseat: number;
-  lokasiberangkat: string;
-};
+export type hargaProps = {
+  tipe: string,
+  nominal: number,
+  currency: string,
+}
 
-const CardPaket = ({
-  paket_id,
-  img,
-  harga,
-  tipe,
-  title,
-  jadwalBerangkat,
-  durasi,
-  hotel,
-  totalseat,
-  remainingseat,
-  lokasiberangkat,
-  
-}: CardProps) => {
+export type hotelProps = {
+  bintang: string,
+  nama_hotel: string,
+  url_hotel: string[],
+}
+
+export type cardProps = {
+  durasi: number,
+  harga: hargaProps[],
+  harga_dp: number,
+  hotel: hotelProps[],
+  img: string,
+  jadwal: string,
+  lokasikeberangkatan: string,
+  maskapai: string,
+  paketID: string,
+  remainingseat: number,
+  thumbnail: string,
+  title: string,
+  totalseat: number,
+}
+
+const CardPaket = ({paketID, img, title}: cardProps) => {
   return (
-    <>
-      <div
-        key={paket_id}
-        className="bg-white border shadow-lg h-fit rounded-xl"
-      >
-        <div className="flexCenter">
-          <Image
-            src={img}
-            alt="paket"
-            width={450}
-            height={450}
-            className="h-36 object-cover rounded-t-xl"
-          />
-        </div>
-        <div className="mx-4">
-          <div className="flexBetween mt-3 mb-2">
-            <p className="font-bold text-[#f14310]">{harga}</p>
-            <p className="font-medium text-[#f14310]">{tipe}</p>
-          </div>
-          <div className="text-center items-center">
-            <h1 className="font-bold text-[16px] text-[#89060b]">{title}</h1>
-          </div>
-          <div className="my-3">
-            <div className="flexBetween items-center text-[13px] md:text-[15px] text-gray-50 my-4">
-              <div className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-gray-50"
-                >
-                  <path d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z" />
-                </svg>
-                <p>Jadwal Keberangkatan</p>
-              </div>
-              <div className="font-bold">
-                <p>{jadwalBerangkat}</p>
-              </div>
-            </div>
-
-            <div className="flexBetween items-center text-[13px] md:text-[15px] text-gray-50 my-4">
-              <div className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-gray-50"
-                >
-                  <path d="M22 14c0 5.523-4.478 10-10 10s-10-4.477-10-10 4.478-10 10-10 10 4.477 10 10zm-2 0c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8 8-3.589 8-8zm-6-11.819v-2.181h-4v2.181c1.408-.238 2.562-.243 4 0zm6.679 3.554l1.321-1.321-1.414-1.414-1.407 1.407c.536.402 1.038.844 1.5 1.328zm-8.679 2.265v6h6c0-3.309-2.691-6-6-6z" />
-                </svg>
-                <p>Durasi Perjalanan</p>
-              </div>
-              <div className="font-bold">
-                <p>{durasi}</p>
-              </div>
-            </div>
-
-            <div className="flexBetween items-center text-[13px] md:text-[15px] text-gray-50 my-4">
-              <div className="flex gap-2 items-center">
-                <svg
-                  width="24"
-                  height="24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  className="fill-gray-50"
-                >
-                  <path d="M21 22h2v2h-22v-2h2v-22h18v22zm-10-3h-2v4h2v-4zm4 0h-2v4h2v-4zm4-17h-14v20h2v-5h10v5h2v-20zm-12 11h2v2h-2v-2zm4 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-8-3h2v2h-2v-2zm4 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-8-3h2v2h-2v-2zm4 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-8-3h2v2h-2v-2zm4 0h2v2h-2v-2zm4 0h2v2h-2v-2z" />
-                </svg>
-                <p>Hotel</p>
-              </div>
-              <div className="font-bold flex items-center gap-2">
-                <svg
-                  width="24"
-                  height="24"
-                  clip-rule="evenodd"
-                  fill-rule="evenodd"
-                  stroke-linejoin="round"
-                  stroke-miterlimit="2"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="fill-gray-50"
-                >
-                  <path
-                    d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44zm.678 2.033-2.361 4.792-5.246.719 3.848 3.643-.948 5.255 4.707-2.505 4.707 2.505-.951-5.236 3.851-3.662-5.314-.756z"
-                    fill-rule="nonzero"
-                  />
-                </svg>
-                <p>{hotel}</p>
-              </div>
-            </div>
-
-            <div className="flexBetween items-center text-[13px] md:text-[15px] text-gray-50 my-4">
-              <div className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-gray-50"
-                >
-                  <path d="M12 2c2.757 0 5 2.243 5 5.001 0 2.756-2.243 5-5 5s-5-2.244-5-5c0-2.758 2.243-5.001 5-5.001zm0-2c-3.866 0-7 3.134-7 7.001 0 3.865 3.134 7 7 7s7-3.135 7-7c0-3.867-3.134-7.001-7-7.001zm6.369 13.353c-.497.498-1.057.931-1.658 1.302 2.872 1.874 4.378 5.083 4.972 7.346h-19.387c.572-2.29 2.058-5.503 4.973-7.358-.603-.374-1.162-.811-1.658-1.312-4.258 3.072-5.611 8.506-5.611 10.669h24c0-2.142-1.44-7.557-5.631-10.647z" />
-                </svg>
-                <p>Total Seat</p>
-              </div>
-              <div className="font-bold">
-                <p>{totalseat} pax</p>
-              </div>
-            </div>
-
-            <div className="flexBetween items-center text-[13px] md:text-[15px] text-gray-50 my-4">
-              <div className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-gray-50"
-                >
-                  <path d="M12 2c2.131 0 4 1.73 4 3.702 0 2.05-1.714 4.941-4 8.561-2.286-3.62-4-6.511-4-8.561 0-1.972 1.869-3.702 4-3.702zm0-2c-3.148 0-6 2.553-6 5.702 0 3.148 2.602 6.907 6 12.298 3.398-5.391 6-9.15 6-12.298 0-3.149-2.851-5.702-6-5.702zm0 8c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm8 12c0 2.209-3.581 4-8 4s-8-1.791-8-4c0-1.602 1.888-2.98 4.608-3.619l1.154 1.824c-.401.068-.806.135-1.178.242-3.312.949-3.453 2.109-.021 3.102 2.088.603 4.777.605 6.874-.001 3.619-1.047 3.164-2.275-.268-3.167-.296-.077-.621-.118-.936-.171l1.156-1.828c2.723.638 4.611 2.016 4.611 3.618z" />
-                </svg>
-                <p>Lokasi Keberangkatan</p>
-              </div>
-              <div className="font-bold">
-                <p>{lokasiberangkat}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-3 mt-6 flex gap-2">
-            <Link href={"/"}>
-              <button className="w-10 h-10 p-1 duration-200 rounded-lg hover:bg-gray-20">
-                <svg
-                  clip-rule="evenodd"
-                  fill-rule="evenodd"
-                  stroke-linejoin="round"
-                  stroke-miterlimit="2"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="m11.25 6c.398 0 .75.352.75.75 0 .414-.336.75-.75.75-1.505 0-7.75 0-7.75 0v12h17v-8.749c0-.414.336-.75.75-.75s.75.336.75.75v9.249c0 .621-.522 1-1 1h-18c-.48 0-1-.379-1-1v-13c0-.481.38-1 1-1zm1.521 9.689 9.012-9.012c.133-.133.217-.329.217-.532 0-.179-.065-.363-.218-.515l-2.423-2.415c-.143-.143-.333-.215-.522-.215s-.378.072-.523.215l-9.027 8.996c-.442 1.371-1.158 3.586-1.264 3.952-.126.433.198.834.572.834.41 0 .696-.099 4.176-1.308zm-2.258-2.392 1.17 1.171c-.704.232-1.274.418-1.729.566zm.968-1.154 7.356-7.331 1.347 1.342-7.346 7.347z"
-                    fill-rule="nonzero"
-                  />
-                </svg>
-              </button>
-            </Link>
-            <Link href={"/"}>
-              <button className="w-10 h-10 p-1 duration-200 rounded-lg hover:bg-gray-20">
-                <svg
-                  clip-rule="evenodd"
-                  fill-rule="evenodd"
-                  stroke-linejoin="round"
-                  stroke-miterlimit="2"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="m4.015 5.494h-.253c-.413 0-.747-.335-.747-.747s.334-.747.747-.747h5.253v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-.254v15.435c0 .591-.448 1.071-1 1.071-2.873 0-11.127 0-14 0-.552 0-1-.48-1-1.071zm14.5 0h-13v15.006h13zm-4.25 2.506c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm-4.5 0c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm3.75-4v-.5h-3v.5z"
-                    fill-rule="nonzero"
-                  />
-                </svg>
-              </button>
-            </Link>
-          </div>
-        </div>
+    <div key={paketID} className='flex flex-col justify-between bg-white h-[19rem] rounded-lg shadow-md border duration-500 hover:shadow-xl'>
+      <div>
+        <img src={img} alt='foto-paket' className='h-40 w-full object-cover rounded-t-lg' />
+        <h1 className='font-semibold text-[14px] text-center my-3 mx-6'>{title}</h1>
       </div>
-    </>
-  );
-};
+      <div className='flexEnd gap-2 items-center m-4'>
+        <Link href={'/'}>
+          <div className='bg-green-50 p-1 rounded-md duration-200 hover:bg-black'>
+            <svg width={28} height={28} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className='fill-white'><path d="m11.25 6c.398 0 .75.352.75.75 0 .414-.336.75-.75.75-1.505 0-7.75 0-7.75 0v12h17v-8.749c0-.414.336-.75.75-.75s.75.336.75.75v9.249c0 .621-.522 1-1 1h-18c-.48 0-1-.379-1-1v-13c0-.481.38-1 1-1zm1.521 9.689 9.012-9.012c.133-.133.217-.329.217-.532 0-.179-.065-.363-.218-.515l-2.423-2.415c-.143-.143-.333-.215-.522-.215s-.378.072-.523.215l-9.027 8.996c-.442 1.371-1.158 3.586-1.264 3.952-.126.433.198.834.572.834.41 0 .696-.099 4.176-1.308zm-2.258-2.392 1.17 1.171c-.704.232-1.274.418-1.729.566zm.968-1.154 7.356-7.331 1.347 1.342-7.346 7.347z"/></svg>
+          </div>
+        </Link>
+        <Link href={'/'}>
+          <div className='bg-red-600 p-1 rounded-md duration-200 hover:bg-black'>
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" className='fill-white'><path d="M19 24h-14c-1.104 0-2-.896-2-2v-17h-1v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2h-1v17c0 1.104-.896 2-2 2zm0-19h-14v16.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-16.5zm-9 4c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm-2-7h-4v1h4v-1z"/></svg>
+          </div>
+        </Link>
+        <Link href={'/'}>
+          <p className='bg-[#f14310] py-1 px-4 font-medium text-white duration-200 hover:bg-black rounded-md'>Detail</p>
+        </Link>
+      </div>
+    </div>
+  )
+}
 
-export default CardPaket;
+export default CardPaket
