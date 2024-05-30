6193b027ac782a0f587dbec845bdb4f670b554af
AlbaytSolo <solo.albayt@gmail.com>
2024-05-30 12:12:26 +0700

    change layout

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -9,67 +9,23 @@ const page = () => {
         <section className='bg-white p-6 rounded-lg'>
             <SectionHead title='Blog' placeholder='Cari Blog' addButton='hidden' link=''/>
 
-            <table className='w-full'>
-                <thead>
-                    <tr className='bg-[#f14310] text-white'>
-                        <th className='p-2 font-semibold text-[14px]'>Blog ID</th>
-                        <th className='p-2 font-semibold text-[14px]'>Judul</th>
-                        <th className='p-2 font-semibold text-[14px]'>Kategori</th>
-                        <th className='p-2 font-semibold text-[14px]'>Penulis</th>
-                        <th className='p-2 font-semibold text-[14px]'>Tanggal Dibuat</th>
-                        <th className='p-2 font-semibold text-[14px]'></th>
-                    </tr>
-                </thead>
-                <tbody>
+            {/* Content */}
+                <div className='grid grid-cols-3 gap-5'>
                     {BERITA_ARTIKEL.map((blog) => (
-                    <tr key={blog.id} className='border-b text-center'>
-                        <td className='p-2 text-[14px]'>{blog.id}</td>
-                        <td className='p-2 text-[14px]'>{blog.title}</td>
-                        <td className='p-2 text-[14px]'>{blog.category}</td>
-                        <td className='p-2 text-[14px]'>{blog.author}</td>
-                        <td className='p-2 text-[14px]'>{blog.date}</td>
-                        <td className='p-2 text-[14px]'>
-                            <div className='flex'>
-                                <Link href={"/"}>
-                                    <button className="w-8 h-8 p-1 duration-200 rounded-lg hover:bg-gray-20">
-                                        <svg
-                                        clip-rule="evenodd"
-                                        fill-rule="evenodd"
-                                        stroke-linejoin="round"
-                                        stroke-miterlimit="2"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                        <path
-                                            d="m11.25 6c.398 0 .75.352.75.75 0 .414-.336.75-.75.75-1.505 0-7.75 0-7.75 0v12h17v-8.749c0-.414.336-.75.75-.75s.75.336.75.75v9.249c0 .621-.522 1-1 1h-18c-.48 0-1-.379-1-1v-13c0-.481.38-1 1-1zm1.521 9.689 9.012-9.012c.133-.133.217-.329.217-.532 0-.179-.065-.363-.218-.515l-2.423-2.415c-.143-.143-.333-.215-.522-.215s-.378.072-.523.215l-9.027 8.996c-.442 1.371-1.158 3.586-1.264 3.952-.126.433.198.834.572.834.41 0 .696-.099 4.176-1.308zm-2.258-2.392 1.17 1.171c-.704.232-1.274.418-1.729.566zm.968-1.154 7.356-7.331 1.347 1.342-7.346 7.347z"
-                                            fill-rule="nonzero"
-                                        />
-                                        </svg>
-                                    </button>
-                                    </Link>
-                                    <Link href={"/"}>
-                                    <button className="w-8 h-8 p-1 duration-200 rounded-lg hover:bg-gray-20">
-                                        <svg
-                                        clip-rule="evenodd"
-                                        fill-rule="evenodd"
-                                        stroke-linejoin="round"
-                                        stroke-miterlimit="2"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                        <path
-                                            d="m4.015 5.494h-.253c-.413 0-.747-.335-.747-.747s.334-.747.747-.747h5.253v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-.254v15.435c0 .591-.448 1.071-1 1.071-2.873 0-11.127 0-14 0-.552 0-1-.48-1-1.071zm14.5 0h-13v15.006h13zm-4.25 2.506c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm-4.5 0c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm3.75-4v-.5h-3v.5z"
-                                            fill-rule="nonzero"
-                                        />
-                                        </svg>
-                                    </button>
-                                </Link>
+                        <Link href={'/DetailBlog'}>
+                            <div key={blog.id} className='bg-white h-64 border shadow-md rounded-lg duration-200 hover:shadow-xl'>
+                                <img src={blog.image} alt='foto-blog' className='object-cover w-full h-[60%] rounded-t-lg' />
+
+                                <div className='px-6 py-3'>
+                                    <h1 className='font-bold text-[18px] text-center'>{blog.title}</h1>
+                                    <p className='text-[14px] text-gray-50 text-center'>{blog.author}</p>
+                                    <p className='text-[14px] text-gray-50 text-center'>{blog.date}</p>
+                                </div>
                             </div>
-                        </td>
-                    </tr>
+                        </Link>
                     ))}
-                </tbody>
-            </table>
+                </div>
+            {/* End Of Content */}
         </section>
     </>
   )
